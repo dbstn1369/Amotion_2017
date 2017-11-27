cd6dc972cbf92a3ebb3f6843a8f3e2ede5980d3c
arcane1028 <arcane1028@gmail.com>
2017-11-28 00:53:53 +0900

    new Async

---
+++ app/src/main/java/com/amotion/amotion_2017/SubjectTableAsyncTask.java
@@ -0,0 +1,27 @@
+package com.amotion.amotion_2017;
+
+import android.os.AsyncTask;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by JSH on 2017-11-27.
+ */
+
+public class SubjectTableAsyncTask extends AsyncTask<Map<String, String>, Subject, ArrayList<Subject>>
+{
+    @Override
+    protected void onProgressUpdate(Subject... values)
+    {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected ArrayList<Subject> doInBackground(Map<String, String>[] maps)
+    {
+
+        publishProgress();
+        return null;
+    }
+}
